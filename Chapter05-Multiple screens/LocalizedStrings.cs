6022517c420df605bcd7830c879780d076443420
hpcodoc <12520324@gm.uit.edu.vn>
2015-05-25 00:52:42 +0700

    +Chapter05-Multiple screens: Many screen of image on a software.

---
+++ Chapter05-Multiple screens/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Chapter05_Multiple_screens.Resources;
+
+namespace Chapter05_Multiple_screens
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
